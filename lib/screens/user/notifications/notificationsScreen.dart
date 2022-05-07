d91384c3ba80f5856ad33e51cde01f4b50a3ecb3
ahmedelmwafy <ahmedelmwafy@gmail.com>
2022-05-07 12:22:32 +0200

    edit notification design

--- lib/screens/user/notifications/notificationsScreen.dart
+++ lib/screens/user/notifications/notificationsScreen.dart
@@ -28,23 +28,23 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
   Widget build(BuildContext context) {
     userEmail = Provider.of<UserData>(context).userEmail;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: const Text(
-          'الاشعارات',
-          style: TextStyle(color: Colors.black),
-        ),
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: const Icon(
-            Icons.arrow_back,
-            color: Colors.white,
-          ),
-        ),
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.white,
+      //   elevation: 0,
+      //   title: const Text(
+      //     'الاشعارات',
+      //     style: TextStyle(color: Colors.black),
+      //   ),
+      //   leading: GestureDetector(
+      //     onTap: () {
+      //       Navigator.pop(context);
+      //     },
+      //     child: const Icon(
+      //       Icons.arrow_back,
+      //       color: Colors.white,
+      //     ),
+      //   ),
+      // ),
       body: StreamBuilder<QuerySnapshot>(
         stream: _store.loadOrders(),
         builder: (context, snapshot) {
@@ -69,66 +69,85 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
             }
             orders.sort((a, b) => a.oCreatedDate.compareTo(b.oCreatedDate));
             List<Order> finalOrderList = orders.reversed.toList();
-            return ListView.builder(
-              itemBuilder: (context, index) => Visibility(
-                // visible: orders[index].isReviewed && !orders[index].isAccepted
-                //     ? false
-                //     : true,
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * .2,
+            return Column(
+              children: [
+                Container(
+                    padding: const EdgeInsets.all(10),
+                    width: double.infinity,
+                    margin: EdgeInsets.only(top: 40, left: 20, right: 20),
                     decoration: BoxDecoration(
-                      color: finalOrderList[index].oIsReviewed
-                          ? finalOrderList[index].oIsAccepted
-                              ? const Color.fromARGB(255, 24, 255, 71)
-                              : const Color.fromARGB(255, 255, 84, 71)
-                          : kSecondaryColor,
-                      borderRadius: BorderRadius.circular(15),
+                      color: kThiredColor,
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            'بريد المستخدم : ${finalOrderList[index].oUserEmail}',
-                            style: const TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w600),
+                    child: Text(
+                      " حالة الطلب",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    )),
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (context, index) => Visibility(
+                      // visible: orders[index].isReviewed && !orders[index].isAccepted
+                      //     ? false
+                      //     : true,
+                      child: Container(
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          
+                          color: finalOrderList[index].oIsReviewed
+                              ? finalOrderList[index].oIsAccepted
+                                  ? const Color.fromARGB(255, 24, 255, 71)
+                                  : const Color.fromARGB(255, 255, 84, 71)
+                              : kSecondaryColor,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                'بريد المستخدم : ${finalOrderList[index].oUserEmail}',
+                                style: const TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w600),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                'كود الطلب : ${finalOrderList[index].documentId}',
+                                style: const TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w600),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                  'حالة الطلب: ${finalOrderList[index].oIsAccepted && finalOrderList[index].oIsReviewed ? 'تم قبول' : !finalOrderList[index].oIsAccepted && finalOrderList[index].oIsReviewed ? 'تم رفض' : 'قيض المراجعة'}',
+                                  style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold)),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                  'تارخ الطلب: ${format.format(DateTime.fromMillisecondsSinceEpoch(finalOrderList[index].oCreatedDate))}',
+                                  style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold)),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                            ],
                           ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            'كود الطلب : ${finalOrderList[index].documentId}',
-                            style: const TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w600),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                              'حالة الطلب: ${finalOrderList[index].oIsAccepted && finalOrderList[index].oIsReviewed ? 'تم قبول' : !finalOrderList[index].oIsAccepted && finalOrderList[index].oIsReviewed ? 'تم رفض' : 'قيض المراجعة'}',
-                              style: const TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold)),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                              'تارخ الطلب: ${format.format(DateTime.fromMillisecondsSinceEpoch(finalOrderList[index].oCreatedDate))}',
-                              style: const TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold)),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                        ],
+                        ),
                       ),
                     ),
+                    itemCount: orders.length,
                   ),
                 ),
-              ),
-              itemCount: orders.length,
+              ],
             );
           }
         },
